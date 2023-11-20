9c0e0bfe35f3d98d70c0354fcf7be19a0c81ad36
Bobák Kornél <143836544+dianne-x@users.noreply.github.com>
2023-11-20 11:52:13 +0100

    a
    a

---
+++ Assets/Scripts/FlyBehavior.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class FlyBehavior : MonoBehaviour
+{
+    [SerializeField] private float _velocity = 1.5f;
+    [SerializeField] private float _rotationSpeed = 10f;
+    private Rigidbody2D _rigidbody2d;
+    
+    private void Start()
+    {
+        _rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    
+    private void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            _rigidbody2d.velocity = Vector2.up * _velocity;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, _rigidbody2d.velocity.y * _rotationSpeed);
+    }
+}
